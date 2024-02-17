cb46b258284d821f98f5d67179931c0193217313
iamsaugat07 <saugatpandey03@gmail.com>
2024-02-17 16:29:22 +0545

    added prettier extension

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,18 +1,18 @@
-import { Body, Controller, Post } from "@nestjs/common";
-import { AuthService } from "./auth.service";
-import { AuthDto } from "./dto";
-import { dot } from "node:test/reporters";
+import { Body, Controller, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { AuthDto } from './dto';
+import { dot } from 'node:test/reporters';
 
 @Controller('auth')
 export class AuthController {
-    constructor(private authService: AuthService) { }
+  constructor(private authService: AuthService) {}
 
-    @Post('signup')
-    signup(@Body() dto: AuthDto) {
-        return this.authService.signup(dto)
-    }
-    @Post('signin')
-    signin(@Body() dto: AuthDto) {
-        return this.authService.signin(dto)
-    }
+  @Post('signup')
+  signup(@Body() dto: AuthDto) {
+    return this.authService.signup(dto);
+  }
+  @Post('signin')
+  signin(@Body() dto: AuthDto) {
+    return this.authService.signin(dto);
+  }
 }
